59e106e7a39deec01e3cf946d935bd62bdfaf249
Alexis <alexisvt@gmail.com>
2017-01-11 18:01:11 -0600

    adding class expression sample

--- Classes/src/class-expression.js
+++ Classes/src/class-expression.js
@@ -1,2 +1,25 @@
 // @flow
 
+let PersonClass = class {
+
+  name: string;
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  sayName() {
+    console.log(this.name);
+  }
+};
+
+let person = new PersonClass('Alexis');
+person.sayName();
+
+console.log(person instanceof PersonClass); // true
+
+console.log(person instanceof Object); // true
+
+console.log(typeof PersonClass); // function
+
+console.log(typeof PersonClass.prototype.sayName); //  function
